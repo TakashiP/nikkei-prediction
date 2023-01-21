d7a6ad2b79839b6c878d1a56a99d88323dbf5100
TakashiP <masai.takashi@gmail.com>
2023-01-21 10:45:25 +0900

    modified timezone into JST and added date and time into index.html

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-from datetime import date, timedelta
+from datetime import date, datetime
 import os
 import sqlite3
 import sys
@@ -12,14 +12,17 @@ bootstrap = Bootstrap(app)
 import Nikkei_10_utilized
 import db
 
-today = date.today()
-
 DATABASE = 'database.db'
 db.create_actual_table()
 db.create_prediction_table()
 
 ## ここからスケジューラー
 from flask_apscheduler import APScheduler
+import pytz
+
+jst = pytz.timezone('Asia/Tokyo')
+today_time = datetime.now().astimezone(jst)
+today = today_time.date()
 
 # set configuration values
 class Config:
@@ -33,9 +36,11 @@ scheduler = APScheduler()
 # scheduler.api_enabled = True
 scheduler.init_app(app)
 
-@scheduler.task('cron', id='do_job_1', hour=7)
+@scheduler.task('cron', id='do_job_1', hour='7', minute='00', timezone=jst)
 def nikkei_prediction():
     Nikkei_10_utilized.predict()
+    today_time = datetime.now().astimezone(jst)
+    today = today_time.date()
 
 scheduler.start()
 ## ここまでスケジューラー
@@ -53,12 +58,17 @@ def index():
     con.close()
     print(actual_data)
 
+    d_html = today_time.strftime('%#m/%#d')
+    t_html = today_time.strftime('%#H:%#M')
+
     data = []
     for row in actual_data:
         data.append({'Date':row[0], 'Nikkei':"{:,.0f}".format(row[1]), 'predict':"{:,.0f}".format(row[2]), 'gap':"{:,.0f}".format(row[3]), 'Nikkei_dod':"{:,.0f}".format(row[4]), 'predict_dod':"{:,.0f}".format(row[5]), 'direction_check':row[6]})
 
     return render_template(
         'index.html',
+        d_html=d_html,
+        t_html=t_html,
         predict=predict,
         data=data
     )
